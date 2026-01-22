d93e6c02871d7b0a7a7704a076413bd05ffc6ac2
David I. Lehn <dlehn@digitalbazaar.com>
2026-01-22 17:37:06 -0500

    Initial files.

---
+++ bin/serialize.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/*!
+ * Copyright (c) 2023-2026 Digital Bazaar, Inc. All rights reserved.
+ */
+import fs from 'node:fs';
+import {metadata} from '../lib/index.js';
+
+// Serialize the contexts as JSON-LD
+for(const {fileUrl, context} of metadata.values()) {
+  fs.writeFileSync(fileUrl, JSON.stringify(context, null, 2) + '\n');
+}
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2019-2026 Digital Bazaar, Inc. All rights reserved.
+ */
+export default {
+  output: {
+    libraryTarget: 'commonjs',
+    filename: 'main.cjs'
+  },
+  mode: 'production',
+  entry: './lib/index.js',
+  module: {
+    parser: {
+      javascript: {
+        url: false
+      }
+    }
+  }
+};
